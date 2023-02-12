eed2120a5a48c0412934d05062eaa3e4fa243870
John Maxwell <john.maxwell@gsa.gov>
2023-02-12 12:30:58 -0500

    Spec cleanup

--- tv/spec/test_data_spec.rb
+++ tv/spec/test_data_spec.rb
@@ -13,35 +13,11 @@ RSpec.describe TestData do
     let(:test_id) { './spec/some_spec.rb[1:1:1:1]' }
 
     let(:test_json) do
-      JSON.generate(
-        {
-          version: '3.12.0',
-          examples: [
-            {
-              id: test_id,
-              description: 'is tested',
-              full_description: 'SomeClass#method is tested',
-              status: 'passed',
-              file_path: './spec/some_spec.rb',
-              line_number: 13,
-              run_time: 0.011690498,
-              pending_message: nil
-            }
-          ],
-          summary: {
-            duration: 0.020395725,
-            example_count: 1,
-            failure_count: 0,
-            pending_count: 0,
-            errors_outside_of_examples_count: 0
-          },
-          summary_line: '3 examples, 0 failures'
-        }
-      )
+      JSON.generate({ examples: [{ id: test_id }] })
     end
 
-    before do
-      @local_run_id = TestData.new.import(test_json, { git_hash: fake_git_hash })
+    let(:local_run_id) do
+      TestData.new.import(test_json, { git_hash: fake_git_hash })
     end
 
     it 'returns [] for a bogus test id' do
@@ -49,20 +25,13 @@ RSpec.describe TestData do
     end
 
     it 'retrieves the test run data' do
+      expected_run_data = Example.new(
+        local_run_id: local_run_id,
+        id: test_id
+      )
+
       expect(TestData.new.test_runs_by_id(test_id)).to eq(
-        [
-          Example.new(
-            local_run_id: @local_run_id,
-            id: test_id,
-            description: 'is tested',
-            full_description: 'SomeClass#method is tested',
-            status: 'passed',
-            file_path: './spec/some_spec.rb',
-            line_number: 13,
-            run_time: 0.011690498,
-            pending_message: nil
-          )
-        ]
+        [expected_run_data]
       )
     end
   end
